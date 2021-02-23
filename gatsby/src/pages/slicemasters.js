b165c08cdea2591902822ec7317e10b0b569d801
Danger <51889207+DangerDan88@users.noreply.github.com>
2021-02-23 11:25:06 -0700

    added slicemaster data from sanity need to work on pagnitation next

--- gatsby/src/pages/slicemasters.js
+++ gatsby/src/pages/slicemasters.js
@@ -1,13 +1,84 @@
 import React from 'react';
+// Link has to be imported as such when you have mutiple imports auto format messed it up.
+import { graphql, Link } from 'gatsby';
+import Img from 'gatsby-image';
+import styled from 'styled-components';
 
-function SliceMastersPage() {
+const SliceMasterGrid = styled.div`
+  display: grid;
+  grid-gap: 2rem;
+  grid-template-columns: repeat(auto-fill, minmax(250px, 1fr));
+`;
+
+const SliceMasterStyles = styled.div`
+  a {
+    text-decoration: none;
+  }
+  .gatsby-image-wrapper {
+    height: 400px;
+  }
+  h2 {
+    transform: rotate(-2deg);
+    text-align: center;
+    font-size: 4rem;
+    margin-bottom: --2rem;
+    position: relative;
+    z-index: 2;
+  }
+  .description {
+    background: var(--yellow);
+    padding: 1rem;
+    margin: 2rem;
+    margin-top: -6rem;
+    z-index: 2;
+    position: relative;
+    transform: rotate(1deg);
+    text-align: center;
+  }
+`;
+// gatsby image wrapper is the tag that gatsby puts on you need to use this tag to select an image for styling
+export default function SlicemastersPage({ data }) {
+  const slicemasters = data.slicemasters.nodes;
+  console.log(slicemasters.name);
+
   return (
-    <div>
-      <h1>SliceMasters page</h1>
-    </div>
+    <>
+      <SliceMasterGrid>
+        {slicemasters.map((person) => (
+          <SliceMasterStyles>
+            <Link to={`/slicemaster/${person.slug.current}`}>
+              <h2>
+                <span className="mark">{person.name}</span>
+              </h2>
+            </Link>
+            <Img fluid={person.image.asset.fluid} />
+            <p className="description">{person.description}</p>
+          </SliceMasterStyles>
+        ))}
+      </SliceMasterGrid>
+    </>
   );
 }
 
-export default SliceMastersPage;
-
-// figure out whyy layout in gatsby browser is not working
+export const query = graphql`
+  query {
+    slicemasters: allSanityPerson {
+      totalCount
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        description
+        image {
+          asset {
+            fluid(maxWidth: 410) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
